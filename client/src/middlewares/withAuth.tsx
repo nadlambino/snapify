e6729d525d94e2290d8136e31e18aa2d123bad00
Ronald Comptente Lambino <rclambino@svengroup.com>
2023-04-08 12:32:46 +0800

    Remove unnecessary import

--- client/src/middlewares/withAuth.tsx
+++ client/src/middlewares/withAuth.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useCookies } from 'react-cookie'
 import UnstrictReactPropType from '../types/UnstrictReactPropType';
