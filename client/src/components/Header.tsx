b6076e6001b48a834adc1e42b2fbacca7da043a2
Ronald Comptente Lambino <rclambino@svengroup.com>
2023-04-15 14:54:03 +0800

    Add settings page

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -17,15 +17,23 @@ export default function Header() {
     navigate('/auth')
   }
 
+  const handleFeedClick = () => {
+    navigate('/')
+  }
+
+  const handleSettingsClick = () => {
+    navigate('settings')
+  }
+
   return (
     <Grid container justifyContent="center" className='header'>
       <Grid item xs={12} sm={7} md={5} lg={4} padding={2} paddingTop={1}>
         <div className='flex items-center justify-between'>
           <Typography>App Name</Typography>
-          <CgFeed size={23} className='btn-icon' />
+          <CgFeed size={23} className='btn-icon' onClick={handleFeedClick} />
           <BsSearch size={21} className='btn-icon' />
           <CgProfile size={23} className='btn-icon' />
-          <FiSettings size={23} className='btn-icon' />
+          <FiSettings size={23} className='btn-icon' onClick={handleSettingsClick}/>
           <RiLogoutCircleRLine size={23} className='btn-icon' onClick={handleSignOut}/>
         </div>
       </Grid>
---
+++ client/src/pages/Settings.tsx
@@ -0,0 +1,106 @@
+import { Grid } from '@mui/material'
+import Header from './../components/Header'
+import Footer from './../components/Footer'
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import MuiAccordion, { AccordionProps } from '@mui/material/Accordion';
+import MuiAccordionSummary, {
+  AccordionSummaryProps,
+} from '@mui/material/AccordionSummary';
+import MuiAccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+
+const Accordion = styled((props: AccordionProps) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  '&:not(:last-child)': {
+    borderBottom: 0,
+  },
+  '&:before': {
+    display: 'none',
+  },
+}));
+
+const AccordionSummary = styled((props: AccordionSummaryProps) => (
+  <MuiAccordionSummary
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+    {...props}
+  />
+))(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === 'dark'
+      ? 'rgba(255, 255, 255, .05)'
+      : 'rgba(0, 0, 0, .03)',
+  flexDirection: 'row-reverse',
+  '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+    transform: 'rotate(90deg)',
+  },
+  '& .MuiAccordionSummary-content': {
+    marginLeft: theme.spacing(1),
+  },
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  padding: theme.spacing(2),
+  borderTop: '1px solid rgba(0, 0, 0, .125)',
+}));
+
+export default function Settings() {
+  const [expanded, setExpanded] = React.useState<string | false>('profile-info');
+
+  const handleChange = (panel: string) => (event: React.SyntheticEvent, newExpanded: boolean) => {
+      setExpanded(newExpanded ? panel : false);
+    };
+
+  return (
+    <Grid item xs={12} sm={7} md={5} lg={4} className="app">
+      <Header />
+      <Grid item container xs={12} sm={7} md={5} lg={4} className='body'>
+        <Grid item>
+          <Accordion expanded={expanded === 'profile-info'} onChange={handleChange('profile-info')}>
+            <AccordionSummary aria-controls="profile-info-content" id="profile-info-header">
+              <Typography>Profile Photo</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
+                sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                sit amet blandit leo lobortis eget.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion expanded={expanded === 'personal-info'} onChange={handleChange('personal-info')}>
+            <AccordionSummary aria-controls="personal-info-content" id="personal-info-header">
+              <Typography>Personal Information</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
+                sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                sit amet blandit leo lobortis eget.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion expanded={expanded === 'account-info'} onChange={handleChange('account-info')}>
+            <AccordionSummary aria-controls="account-info-content" id="account-info-header">
+              <Typography>Account Information</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
+                sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                sit amet blandit leo lobortis eget.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </Grid>
+      </Grid>
+      <Footer />
+    </Grid>
+  )
+}
