f1a8ebdc6555e6f0e8272cef8a579f5d7e756258
Ronald Comptente Lambino <rclambino@svengroup.com>
2023-04-15 11:18:21 +0800

    Show error message on failed login

--- client/src/components/Auth/SignIn.tsx
+++ client/src/components/Auth/SignIn.tsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { FormEvent, useState } from 'react'
+import { useDispatch } from 'react-redux'
 import { useCookies } from 'react-cookie'
 import { setAuth } from './../../store/modules/auth'
-import { Grid, TextField, Button } from '@mui/material'
+import { Grid, TextField, Button, Typography } from '@mui/material'
 import UnstrictReactPropType from '../../types/UnstrictReactPropType'
 import { signIn } from '../../api/auth'
 import { useNavigate } from 'react-router-dom'
 
 export default function SignIn(props: UnstrictReactPropType) {
-  const auth = useSelector((state: any) => (state.auth))
   const navigate = useNavigate()
   const [cookies, setCookie] = useCookies()
+  const [error, setError] = useState<String>()
 
   const [form, setForm] = useState<{email: string | null, password: string | null}>({
     email: null,
@@ -23,33 +23,43 @@ export default function SignIn(props: UnstrictReactPropType) {
     setForm((prevState) => ({...prevState, [key]: value}))
   }
 
-  const handleSignUp = async () => {
+  const handleSignUp = async (e: FormEvent) => {
+    e.preventDefault()
+    setError('')
     const user = await signIn(form)
 
-    if (user) {
-      dispatch(setAuth(user))
-      setCookie('token', user.access.token, {expires: new Date(user.access.expiration)})
-      setCookie('user', user.user, {expires: new Date(user.access.expiration)})
-      navigate('/')
+    if (!user) {
+      setError('Failed to login. Please check your credentials.')
+      return
     }
+
+    dispatch(setAuth(user))
+    setCookie('token', user.access.token, {expires: new Date(user.access.expiration)})
+    setCookie('user', user.user, {expires: new Date(user.access.expiration)})
+    navigate('/')
   }
 
   return (
     <Grid item xs={12}>
-      <Grid container justifyContent="center" direction="column" padding={5} gap={2}>
-        <Grid item xs={12}>
-          <TextField variant="outlined" label="Email" type="email" fullWidth onChange={(e) => handleFormChange('email', e.target.value)} />
+      <form onSubmit={handleSignUp}>
+        <Grid container justifyContent="center" direction="column" padding={5} gap={2}>
+          <Grid item xs={12}>
+            <Typography textAlign="center" className="text-gray-500">{error}</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField variant="outlined" label="Email" type="email" fullWidth onChange={(e) => handleFormChange('email', e.target.value)} />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField variant="outlined" label="Password" type="password" fullWidth onChange={(e) => handleFormChange('password', e.target.value)} />
+          </Grid>
+          <Grid item xs={12}>
+            <Button variant="contained" fullWidth className="btn bg-primary" type="submit">Sign In</Button>
+          </Grid>
+          <Grid item xs={12}>
+            {props.children && props.children}
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <TextField variant="outlined" label="Password" type="password" fullWidth onChange={(e) => handleFormChange('password', e.target.value)} />
-        </Grid>
-        <Grid item xs={12}>
-          <Button variant="contained" fullWidth className="btn bg-primary" onClick={handleSignUp}>Sign In</Button>
-        </Grid>
-        <Grid item xs={12}>
-          {props.children && props.children}
-        </Grid>
-      </Grid>
+      </form>
     </Grid>
   )
 }
