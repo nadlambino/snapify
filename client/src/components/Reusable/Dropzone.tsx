8e68f61dbbe123751305f21f9631cbd29771831f
Ronald Comptente Lambino <rclambino@svengroup.com>
2023-04-15 21:06:39 +0800

    Refactor dropzone component to display image and videos

--- client/src/components/Reusable/Dropzone.tsx
+++ client/src/components/Reusable/Dropzone.tsx
@@ -4,7 +4,12 @@ import { MdAddPhotoAlternate } from 'react-icons/md'
 import { useEffect, useState } from 'react';
 
 export default function Dropzone() {
+  const onDrop = (newFiles: File[]) => {
+    console.log(newFiles)
+    setFiles([...files, ...newFiles])
+  }
   const {acceptedFiles, getRootProps, getInputProps} = useDropzone({
+    onDrop,
     accept: {
       'image/*': ['.jpeg', '.jpg', '.webp', '.png'],
       'video/*': ['.mp4']
@@ -13,17 +18,20 @@ export default function Dropzone() {
   });
   const [files, setFiles] = useState<File[]>(acceptedFiles)
 
-  useEffect(() => {
-    setFiles([...files, ...acceptedFiles])
-  }, [acceptedFiles])
-
   return (
-    <section className="flex justify-start w-full h-full gap-2">
+    <section className="flex justify-center w-full h-full gap-2">
       <div className='preview flex flex-wrap overflow-x-auto gap-2 justify-center'>
         {
-          files && files.map(file => (
-            <img key={Math.random()} src={URL.createObjectURL(file)} alt={file.name} />
-          ))
+          files && files.map((file, index) => {
+            return (
+              file.type.includes('image') ?
+              <img key={index} src={URL.createObjectURL(file)} alt={file.name} />
+              :
+              <video key={index} autoPlay muted>
+                <source src={URL.createObjectURL(file)} type="video/mp4"></source>
+              </video>
+            )
+          })
         }
         {
           files.length < 5 ?
