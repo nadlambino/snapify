0eb73c93630d6a08653bdc08a2e5eebe9e0d1c5c
Ronald Comptente Lambino <rclambino@svengroup.com>
2023-04-20 20:19:43 +0800

    Separate post css and refactor post component

--- client/src/components/Post/Post.tsx
+++ client/src/components/Post/Post.tsx
@@ -1,8 +1,9 @@
-import samplePhoto from './../../assets/images/sample-profile.jpg'
 import { PostType, MediaType, UserType } from "../../types/PostType"
 import Media from "./Media"
 import { useEffect, useState } from 'react'
 import dayjs from 'dayjs';
+import { Avatar } from '@mui/material'
+import './../../css/post.css'
 
 interface Props {
   post: PostType
@@ -11,6 +12,8 @@ interface Props {
 export default function Post({ post }: Props ) {
   const media: [MediaType] = post.media
   const user: UserType = post.user
+  const fullName = `${user.firstName} ${user.lastName}`
+  const initials = `${user.firstName[0]}${user.lastName[0]}`
   const timePosted = dayjs(post.createdAt).fromNow()
   const [activeSlide, setActiveSlide] = useState(0)
   let timeout: number = 0
@@ -31,9 +34,9 @@ export default function Post({ post }: Props ) {
     <div className='item'>
       <div className='post-details-container'>
         <div className='overlay-top'></div>
-        <img src={samplePhoto} className='post-profile' alt="Profile Photo"/>
+        <Avatar alt={fullName} sx={{ bgcolor: '#2A2F4F', fontSize: '14px' }}>{initials}</Avatar>
         <div className="flex flex-col">
-          <span className='capitalized text-gray-200 relative z-10'>{`${user.firstName} ${user.lastName}`}</span>
+          <span className='capitalized text-gray-200 relative z-10'>{fullName}</span>
           <small className="text-gray-300">{timePosted}</small>
         </div>
       </div>
@@ -46,7 +49,7 @@ export default function Post({ post }: Props ) {
               className={
                 'slide-thumb-item ' + 
                 (index === activeSlide ? 'slide-active ' : '') +
-                (index < activeSlide ? 'prev-slide' : '')}>
+                (index < activeSlide ? 'slide-prev' : '')}>
               </span>)
           )
         }
@@ -56,7 +59,7 @@ export default function Post({ post }: Props ) {
           media.map((image, index) => <Media media={image} key={image._id} className={index === activeSlide ? 'flex': 'hidden'} />)
         }
         </div>
-        <div className='absolute bottom-0 p-3 w-full'>
+        <div className='absolute bottom-0 p-3 py-6 w-full'>
           <div className='overlay-bottom'></div>
           <span className='text-white'>{post.content}</span>
         </div>
