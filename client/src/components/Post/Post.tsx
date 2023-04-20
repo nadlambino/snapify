fda66ad5b838da808c753ec1628cb1d17c90b6b5
Ronald Comptente Lambino <rclambino@svengroup.com>
2023-04-20 17:12:23 +0800

    revise UI for post

--- client/src/components/Post/Post.tsx
+++ client/src/components/Post/Post.tsx
@@ -1,14 +1,22 @@
-import { Grid } from "@mui/material"
 import samplePhoto from './../../assets/images/sample-profile.jpg'
-import { PostType } from "../../types/PostType"
+import { PostType, MediaType } from "../../types/PostType"
 import Media from "./Media"
+import Stories from 'react-insta-stories';
 
 interface Props {
   post: PostType
 }
 
 export default function Post({ post }: Props ) {
-  const { media } = post
+  const media: [MediaType] = post.media
+  const stories = media.map(data => {
+    const url = window.apiUrl + data.src.replace('public', '')
+    return {
+      url,
+      type: data.category,
+      duration: data.duration
+    }
+  })
 
   return (
     <div className='item'>
@@ -19,10 +27,17 @@ export default function Post({ post }: Props ) {
           <small className="text-gray-400">9 minutes ago</small>
         </div>
       </div>
-      <div className="flex flex-wrap overflow-auto post-image-wrapper">
-      {
-        media.map(image => <Media media={image} key={image._id} />)
-      }
+      <div className='flex flex-wrap overflow-auto post-image-wrapper'>
+        <div className='thumb-container flex bg-blue w-full gap-1 absolute top-0 z-10 p-2'>
+          {
+            media.map((data) => <span key={data._id} className='bg-gray-400 rounded h-[3px] w-full'></span>)
+          }
+        </div>
+        <div className='flex flex-wrap overflow-auto post-image-container'>
+        {
+          media.map(image => <Media media={image} key={image._id} />)
+        }
+        </div>
       </div>
     </div>
   )
