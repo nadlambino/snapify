649cfacfe6cc6ba65b27509350dc22d014be1899
Ronald Comptente Lambino <rclambino@svengroup.com>
2023-05-09 16:19:22 +0800

    Fix feed container

--- client/src/components/Body.tsx
+++ client/src/components/Body.tsx
@@ -4,7 +4,7 @@ import Feed from './Post/Feed'
 
 export default function Body() {
   return (
-    <Grid container item xs={12} sm={7} md={5} lg={4} className='body'>
+    <Grid container item xs={12} sm={7} md={5} lg={4} className='body has-header'>
       <Feed />
     </Grid>
   )
