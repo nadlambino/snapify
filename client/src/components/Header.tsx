eb1e13c826227c08e70497f50ce4f49619472f64
Ronald Lambino <ronaldlambino0000@gmail.com>
2023-07-27 14:44:40 +0800

    Add scroll to top and feed reload

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,16 +1,23 @@
-import { Link, useLocation } from 'react-router-dom'
-import { useCookies } from 'react-cookie'
-import { useNavigate } from 'react-router-dom'
-import { MdLogout, MdOutlineLibraryAdd, MdOutlineDynamicFeed } from 'react-icons/md'
-import { useState } from 'react'
-import Form from './Form'
-import Create from './Post/Create'
+import { Link, useLocation } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
+import {
+  MdLogout,
+  MdOutlineLibraryAdd,
+  MdOutlineDynamicFeed,
+} from 'react-icons/md';
+import { useState } from 'react';
+import Form from './Form';
+import Create from './Post/Create';
+import { useDispatch } from 'react-redux';
+import { setReloadPosts } from '../store/modules/post';
 
 export default function Header() {
-  const [cookies, setCookies, removeCookie] = useCookies()
-  const navigate = useNavigate()
-  const location = useLocation()
+  const [cookies, setCookies, removeCookie] = useCookies();
+  const navigate = useNavigate();
+  const location = useLocation();
   const [open, setOpen] = useState(false);
+  const dispatch = useDispatch();
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -18,40 +25,58 @@ export default function Header() {
 
   const handleClose = () => {
     setOpen(false);
-  }
+  };
 
   const getMenuActiveClass = (path: string) => {
-    return location.pathname === path ? 'active' : ''
-  }
+    return location.pathname === path ? 'active' : '';
+  };
 
   const handleSignOut = () => {
-    removeCookie('token')
-    navigate('/auth')
-  }
+    removeCookie('token');
+    navigate('/auth');
+  };
 
   const handleFeedClick = () => {
-    navigate('/')
-  }
+    navigate('/');
+    dispatch(setReloadPosts(true));
+    window.scrollTo({ top: 0 });
+  };
 
   return (
-    <div className='header'>
-      <Link to={'/'} className='header-logo'>
-        <div className='flex flex-col'>
-          <span className='font-bold text-gray-800'>SNAPIFY</span>
+    <div className="header">
+      <Link
+        to={'/'}
+        onClick={handleFeedClick}
+        className="header-logo"
+      >
+        <div className="flex flex-col">
+          <span className="font-bold text-gray-800">SNAPIFY</span>
         </div>
       </Link>
-      <div className='header-menu'>
-        <MdOutlineDynamicFeed size={30} className={`btn-icon header-btn ${getMenuActiveClass('/')}`} onClick={handleFeedClick} />
-        <MdOutlineLibraryAdd size={30} className={`btn-icon header-btn `} onClick={handleClickOpen}/>
-        <MdLogout size={30} className='btn-icon header-btn' onClick={handleSignOut}/>
+      <div className="header-menu">
+        <MdOutlineDynamicFeed
+          size={30}
+          className={`btn-icon header-btn ${getMenuActiveClass('/')}`}
+          onClick={handleFeedClick}
+        />
+        <MdOutlineLibraryAdd
+          size={30}
+          className={`btn-icon header-btn `}
+          onClick={handleClickOpen}
+        />
+        <MdLogout
+          size={30}
+          className="btn-icon header-btn"
+          onClick={handleSignOut}
+        />
       </div>
-      <Form 
+      <Form
         save="POST"
         saveIcon={<MdOutlineLibraryAdd size={20} />}
-        show={open} 
+        show={open}
         closeCallback={handleClose}
-        component={Create}>
-      </Form>
+        component={Create}
+      ></Form>
     </div>
   );
 }
