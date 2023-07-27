07529a2bc3f74bb32c62e1d3c45eed2570e5f5f3
Ronald Lambino <ronaldlambino0000@gmail.com>
2023-07-27 12:00:23 +0800

    Use dvh

--- client/src/pages/PageNotFound.tsx
+++ client/src/pages/PageNotFound.tsx
@@ -3,7 +3,7 @@ import { Grid, Typography } from '@mui/material'
 export default function PageNotFound() {
   return (
     <div className="app">
-      <Grid container justifyContent="center" alignItems="center" position="fixed" height="calc(100vh - 56px)">
+      <Grid container justifyContent="center" alignItems="center" position="fixed" height="calc(100dvh - 56px)">
         <Grid item xs={12} textAlign="center">
           <Typography variant="h5">The page you are trying to visit might be broken or not existing</Typography>
         </Grid>
