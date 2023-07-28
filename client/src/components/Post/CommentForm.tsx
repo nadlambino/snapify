3f03a74fbd9b72f2fe9d3b73aa36b6938ad58853
Ronald Lambino <ronaldlambino0000@gmail.com>
2023-07-28 11:49:28 +0800

    Refactor comment

--- client/src/components/Post/CommentForm.tsx
+++ client/src/components/Post/CommentForm.tsx
@@ -1,28 +1,37 @@
-import './../../css/comments.css'
-import { TextField } from '@mui/material'
-import { AiOutlineSend } from 'react-icons/ai'
-import { commentPost } from '../../api/post'
-import { useState } from 'react'
-import dayjs from 'dayjs'
-import { FormEvent } from 'react'
-import useAuth from '../../hooks/auth'
+import './../../css/comments.css';
+import { TextField } from '@mui/material';
+import { AiOutlineSend } from 'react-icons/ai';
+import { commentPost } from '../../api/post';
+import { useState } from 'react';
+import dayjs from 'dayjs';
+import { FormEvent } from 'react';
+import useAuth from '../../hooks/auth';
+import { useMutation } from 'react-query';
 
 interface Props {
-  postId: string,
-  newCommentCB: Function
+  postId: string;
+  newCommentCB: Function;
 }
 
 export default function CommentForm({ postId, newCommentCB }: Props) {
-  const [comment, setComment] = useState('')
-  const { user } = useAuth()
+  const [comment, setComment] = useState('');
+  const { user } = useAuth();
+  const { mutate, isLoading } = useMutation({
+    mutationFn: () => {
+      return commentPost({ postId, comment: comment.trim() });
+    },
+  });
 
   const handleCommentChange = (value: string) => {
-    setComment(value)
-  }
+    setComment(value);
+  };
 
-  const handleCommentSend = (e: FormEvent<HTMLFormElement | HTMLButtonElement>) => {
-    e.preventDefault()
-    if (comment.length) {
+  const handleCommentSend = (
+    e: FormEvent<HTMLFormElement | HTMLButtonElement>
+  ) => {
+    e.preventDefault();
+    if (isLoading) return;
+    if (comment.trim().length) {
       newCommentCB({
         content: comment,
         createdAt: dayjs().format('YYYY-MM-DDTHH:mm:ss.sssZ'),
@@ -32,29 +41,36 @@ export default function CommentForm({ postId, newCommentCB }: Props) {
           _id: user?._id || '',
           firstName: user?.firstName || 'Someone',
           lastName: user?.lastName || '',
-          email: user?.email || ''
-        }
-      })
-      commentPost({ postId, comment }).then(() => {
-        setComment('')
+          email: user?.email || '',
+        },
       });
+
+      setComment('');
+      mutate();
     }
-  }
+  };
 
   return (
-    <div className='comment-form'>
+    <div className="comment-form">
       <form onSubmit={handleCommentSend}>
-        <TextField 
+        <TextField
           value={comment}
           onChange={(e) => handleCommentChange(e.target.value)}
-          placeholder='Say something...'
+          placeholder="Say something..."
           fullWidth
           variant="outlined"
+          disabled={isLoading}
         />
-        <button onSubmit={handleCommentSend}>
-          <AiOutlineSend size={30} className="text-gray-400" />
+        <button
+          onSubmit={handleCommentSend}
+          disabled={isLoading}
+        >
+          <AiOutlineSend
+            size={30}
+            className="text-gray-400"
+          />
         </button>
       </form>
     </div>
-  )
+  );
 }
