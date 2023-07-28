dcc270d3ee2008c4c5ed8ba84449930400f00ef3
Ronald Lambino <ronaldlambino0000@gmail.com>
2023-07-28 10:35:21 +0800

    use react mutation and display better alert

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -1,27 +1,27 @@
-import { AxiosResponse } from "axios"
+import { AxiosResponse } from 'axios';
 
 export type SignInData = {
-  email: string | null, 
-  password: string | null
-}
+  email: string | null;
+  password: string | null;
+};
 
 export type SignUpData = {
-  firstName: string | null,
-  lastName: string | null,
-  gender: 'male' | 'female' | 'other' | null,
-  email: string | null,
-  password: string | null,
-  confirm: string | null
-}
+  firstName: string | null;
+  lastName: string | null;
+  gender: 'male' | 'female' | 'other' | null;
+  email: string | null;
+  password: string | null;
+  confirm: string | null;
+};
 
 export const signIn = async (user: SignInData) => {
-  return await window.axios.post(`${window.apiUrl}/signin`, user)
-    .then((response: AxiosResponse) => response.data)
-    .catch(() => null)
-}
+  return await window.axios
+    .post(`${window.apiUrl}/signin`, user)
+    .then((response: AxiosResponse) => response.data);
+};
 
 export const signUp = async (user: SignUpData) => {
-  return await window.axios.post(`${window.apiUrl}/signup`, user)
-    .then((response: AxiosResponse) => response.data)
-    .catch(() => null)
-}
+  return await window.axios
+    .post(`${window.apiUrl}/signup`, user)
+    .then((response: AxiosResponse) => response.data);
+};
--- client/src/components/Auth/SignIn.tsx
+++ client/src/components/Auth/SignIn.tsx
@@ -1,76 +1,130 @@
-import { FormEvent, useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { useCookies } from 'react-cookie'
-import { setAuth } from './../../store/modules/auth'
-import { Grid, TextField, Button, Typography } from '@mui/material'
-import { SignInData, signIn } from '../../api/auth'
-import { useNavigate } from 'react-router-dom'
-import { Any } from '../../types'
+import { FormEvent, useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useCookies } from 'react-cookie';
+import { setAuth } from './../../store/modules/auth';
+import { Grid, TextField, Button, Typography, Alert } from '@mui/material';
+import { SignInData, signIn } from '../../api/auth';
+import { useNavigate } from 'react-router-dom';
+import { Any } from '../../types';
+import { useMutation } from 'react-query';
 
 export default function SignIn(props: React.PropsWithChildren<Any>) {
-  const navigate = useNavigate()
-  const [_, setCookie] = useCookies()
-  const [error, setError] = useState<String>()
+  const navigate = useNavigate();
+  const [_, setCookie] = useCookies();
+  const dispatch = useDispatch();
+  const { mutate, isLoading, isError, isSuccess, data, error } = useMutation({
+    mutationFn: (form: SignInData) => signIn(form),
+  });
 
   const [form, setForm] = useState<SignInData>({
     email: null,
-    password: null
-  })
+    password: null,
+  });
 
-  const dispatch = useDispatch()
+  useEffect(() => {
+    if (data && isSuccess && !isLoading) {
+      dispatch(setAuth(data));
+      setCookie('token', data.access.token, {
+        expires: new Date(data.access.expiration),
+      });
+      setCookie('user', data.user, {
+        expires: new Date(data.access.expiration),
+      });
+      navigate('/');
+    }
+  }, [data, isSuccess, isLoading]);
 
   const handleFormChange = (key: string, value: string | null) => {
-    setForm((prevState) => ({...prevState, [key]: value}))
-  }
+    setForm((prevState) => ({ ...prevState, [key]: value }));
+  };
 
   const handleSignUp = async (e: FormEvent) => {
-    e.preventDefault()
-    setError('')
-    signInUser(form)
-  }
+    e.preventDefault();
+    mutate(form);
+  };
 
   const handleSignInDemo = () => {
-    signInUser({ email: 'johndoe@test.com', password: 'password'})
-  }
-
-  const signInUser = async (form : SignInData) => {
-    const user = await signIn(form)
-
-    if (!user) {
-      setError('Failed to login. Please check your credentials.')
-      return
-    }
-
-    dispatch(setAuth(user))
-    setCookie('token', user.access.token, {expires: new Date(user.access.expiration)})
-    setCookie('user', user.user, {expires: new Date(user.access.expiration)})
-    navigate('/')
-  }
+    mutate({ email: 'johndoe@test.com', password: 'password' });
+  };
 
   return (
-    <div className='w-full flex flex-col justify-center'>
+    <div className="w-full flex flex-col justify-center">
       <form onSubmit={handleSignUp}>
-        <Grid container justifyContent="center" direction="column" gap={2}>
-          <Grid item xs={12}>
-            <Typography textAlign="center" className="text-gray-500">{error}</Typography>
+        <Grid
+          container
+          justifyContent="center"
+          direction="column"
+          gap={2}
+        >
+          {isError && (
+            <Grid
+              item
+              xs={12}
+            >
+              <Alert severity="error">
+                Failed to sign in. Please check your credentials
+              </Alert>
+            </Grid>
+          )}
+          <Grid
+            item
+            xs={12}
+          >
+            <TextField
+              variant="outlined"
+              label="Email"
+              type="email"
+              fullWidth
+              onChange={(e) => handleFormChange('email', e.target.value)}
+            />
           </Grid>
-          <Grid item xs={12}>
-            <TextField variant="outlined" label="Email" type="email" fullWidth onChange={(e) => handleFormChange('email', e.target.value)} />
+          <Grid
+            item
+            xs={12}
+          >
+            <TextField
+              variant="outlined"
+              label="Password"
+              type="password"
+              fullWidth
+              onChange={(e) => handleFormChange('password', e.target.value)}
+            />
           </Grid>
-          <Grid item xs={12}>
-            <TextField variant="outlined" label="Password" type="password" fullWidth onChange={(e) => handleFormChange('password', e.target.value)} />
+          <Grid
+            item
+            xs={12}
+          >
+            <Button
+              variant="contained"
+              fullWidth
+              className="btn bg-primary"
+              type="submit"
+            >
+              Sign In
+            </Button>
           </Grid>
-          <Grid item xs={12}>
-            <Button variant="contained" fullWidth className="btn bg-primary" type="submit">Sign In</Button>
+          <Grid
+            item
+            xs={12}
+          >
+            <Button
+              variant="contained"
+              fullWidth
+              className="btn bg-secondary"
+              type="button"
+              onClick={handleSignInDemo}
+            >
+              Sign In as Demo
+            </Button>
           </Grid>
-          <Grid item xs={12}>
-            <Button variant="contained" fullWidth className="btn bg-secondary" type="button" onClick={handleSignInDemo}>Sign In as Demo</Button>
-          </Grid>
-          <Grid item xs={12}>
+          <Grid
+            item
+            xs={12}
+          >
             {props.children && props.children}
           </Grid>
         </Grid>
       </form>
     </div>
-  )
+  );
 }
