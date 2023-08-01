f4f1d6cb7ee0206df0595798f7aa0aae7f95c2ef
Ronald Lambino <ronaldlambino0000@gmail.com>
2023-08-01 16:24:32 +0800

    Fix duplicate posts

--- client/src/components/Post/Feed.tsx
+++ client/src/components/Post/Feed.tsx
@@ -1,11 +1,12 @@
 import Post from './Post';
 import { MAX_POST_PER_REQUEST, getPosts } from '../../api/post';
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useInfiniteQuery } from 'react-query';
 import { useTimeout } from '@mantine/hooks';
 import { PostType } from '../../types';
 
 export default function Feed() {
+  const [posts, setPosts] = useState<PostType[]>([]);
   const { data, fetchNextPage } = useInfiniteQuery(
     'post',
     ({ pageParam }) => {
@@ -13,10 +14,6 @@ export default function Feed() {
     },
     { enabled: false }
   );
-  // TODO: Refactor posts collection to filter out duplicate posts
-  const posts = useMemo(() => {
-    return (data?.pages ? data.pages.flat() : []) as PostType[];
-  }, [data]);
   const { start, clear } = useTimeout(([id]) => {
     fetchNextPage({
       pageParam: id,
@@ -27,6 +24,17 @@ export default function Feed() {
   useEffect(() => {
     fetchNextPage();
   }, []);
+
+  useEffect(() => {
+    let p: PostType[] = data?.pages ? data.pages[data.pages.length - 1] : [];
+    const allPosts = posts
+      .concat(p)
+      .filter(
+        (obj, index, self) => index === self.findIndex((t) => t._id === obj._id)
+      );
+
+    setPosts(allPosts);
+  }, [data]);
 
   const handleGetNewPost = async (id: string) => {
     const pages = data?.pages || [];
